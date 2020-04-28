5942c6c34355674055e3dd7d1ec835add12003b2
Raymond Liu <rliu@edgewise.net>
2020-04-28 18:18:41 -0400

    add @ConfigurationPropertiesScan to ExperimentController

--- src/test/java/com/philosobyte/springbootexperiment/ExperimentControllerTest.java
+++ src/test/java/com/philosobyte/springbootexperiment/ExperimentControllerTest.java
@@ -1,9 +1,12 @@
 package com.philosobyte.springbootexperiment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan
 @WebMvcTest(controllers = ExperimentController.class)
 public class ExperimentControllerTest {
     @Test
